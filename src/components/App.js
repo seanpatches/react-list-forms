4eb74257aaa3bb0084ed70394704771d3ebb798e
Sean Nelson <smnelsonn@gmail.com>
2019-05-15 14:11:22 -0700

    made textFormatter

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,6 @@
 import React from 'react';
-import Colors from './Colors.js';
+import TextFormatter from './TextFormatter.js';
 
 export default function App() {
-  const colors = [
-    { name: 'red', rgb: { red: 255, green: 0, blue: 0 } },
-    { name: 'green', rgb: { red: 0, green: 255, blue: 0 } },
-    { name: 'blue', rgb: { red: 0, green: 0, blue: 255 } },
-  ];
-
-  return <Colors colors={colors} />;
+  return <TextFormatter />;
 }
---
+++ src/components/TextFormatter.js
@@ -0,0 +1,24 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
+
+export default class TextFormatter extends PureComponent {
+  state = {
+    text: ''
+  };
+
+  handleChange = ({ target }) => {
+    this.setState({ [target.name]: target.value }, () => {
+      this.text;
+    });
+  };
+  
+  render(){
+    return (
+      <>
+      <input type="text" value={this.state.text} onChange={this.handleChange} />
+      <p text={this.state.text} />
+      </>
+    );
+  }
+}
