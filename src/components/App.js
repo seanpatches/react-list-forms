7c337da0eed1873a8b8cbf4eb2bcbef41a603d06
Sean Nelson <smnelsonn@gmail.com>
2019-05-15 17:16:11 -0700

    changed export

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,11 @@ import Sidebar from './Sidebar.js';
 export default function App() {
 
   return (
-    <section className="Sidebar">
-      <Sidebar />
+    <Sidebar>
       <a>Home</a>
       <a>Space</a>
       <a>Forward</a>
       <a>Stay</a>
-    </section>
+    </Sidebar>
   );
 }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function Sidebar({ children }) {
+function Sidebar({ children }) {
   return (
     <nav>
       {children}
@@ -12,3 +12,5 @@ export default function Sidebar({ children }) {
 Sidebar.propTypes = {
   children: PropTypes.node
 };
+
+export default Sidebar;
