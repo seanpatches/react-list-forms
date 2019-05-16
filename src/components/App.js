60c957cb5a3d83e23e7fd54f182f71516196a0c2
Sean Nelson <smnelsonn@gmail.com>
2019-05-15 17:48:54 -0700

    working / barely styled

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Sidebar from './Sidebar.js';
+import style from './App.css';
 
 export default function App() {
 
+  const [selected, updateSelect] = useState('home');
+
+  const dropdown = {
+    enter: 'Hey there, partner',
+    drums: 'Boom Boom',
+    space: 'wobble',
+    cower: 'too much'
+  };
+
   return (
-    <Sidebar>
-      <a>Home</a>
-      <a>Space</a>
-      <a>Forward</a>
-      <a>Stay</a>
-    </Sidebar>
+    <section className={style.App}>
+      <Sidebar>
+        <a onClick={() => updateSelect('enter')}>Enter</a>
+        <a onClick={() => updateSelect('drums')}>Drums</a>
+        <a onClick={() => updateSelect('space')}>Space</a>
+        <a onClick={() => updateSelect('cower')}>Cower</a>
+      </Sidebar>
+      <p>{dropdown[selected]}</p>
+    </section>
   );
 }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import style from './Sidebar.css';
 
 function Sidebar({ children }) {
   return (
-    <nav>
+    <nav className={style.Sidebar}>
       {children}
     </nav>
   );
