d46109165bce844a05664e14dbaf818291ff2c37
Sean Nelson <smnelsonn@gmail.com>
2019-05-15 17:10:51 -0700

    app start

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Sidebar from './Sidebar.js';
+
+export default function App() {
+
+  return (
+    <section className="Sidebar">
+      <Sidebar />
+      <a>Home</a>
+      <a>Space</a>
+      <a>Forward</a>
+      <a>Stay</a>
+    </section>
+  );
+}
